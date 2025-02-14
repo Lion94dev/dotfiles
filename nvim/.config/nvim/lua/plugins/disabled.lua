68f722d378bea80eaeef5b3e4e0530b2d89dd118
lion <lion@rednose.nl>
2025-02-14 09:18:30 -0300

    chore: disable unsued plugins

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,8 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "tpope/vim-dadbod", enabled = false },
+  { "kristijanhusak/vim-dadbod-ui", enabled = false },
+  { "kristijanhusak/vim-dadbod-completion", enabled = false },
+  { "catppuccin/nvim", enabled = false },
 }

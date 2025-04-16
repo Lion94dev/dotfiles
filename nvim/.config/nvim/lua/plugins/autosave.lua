29c9641cd1af85e8677bcb13406e15fd8a8ba951
lion <lion@rednose.nl>
2025-04-16 11:17:37 -0300

    fix: harpoon window instantly closing, swapped to maintained auto-save fork

--- nvim/.config/nvim/lua/plugins/autosave.lua
+++ nvim/.config/nvim/lua/plugins/autosave.lua
@@ -1,6 +1,14 @@
 return {
-  "Pocco81/auto-save.nvim",
+  "okuuva/auto-save.nvim",
   opts = {
+
+    condition = function(buf)
+      if vim.fn.getbufvar(buf, "&filetype") == "harpoon" then
+        return false
+      end
+      return true
+    end,
+
     execution_message = {
       message = function() -- message to print on save
         return ""
